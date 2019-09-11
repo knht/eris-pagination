91b1f7d4daf51ba52006e6e5d707628fefee2976
D S <contact@circuitrcay.com>
2019-09-11 10:48:44 +1000

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,66 +3,28 @@
 // Author: riyacchi <https://github.com/riyacchi/>
 // Definitions by: CircuitRCAY <https://github.com/CircuitCodes>
 
-import {PaginationEmbed} from "./lib/PaginationEmbed";
-import {Message, EmbedOptions} from "eris";
+declare module 'eris-pagination' {
+    import { Message, EmbedOptions } from 'eris';
 
-export = PaginationEmbed;
+    class PaginationEmbed {
+        constructor(message: Message, pages: EmbedOptions[], options?: PaginationEmbedOptions);
+        public initialize(): Promise<void>;
+        public run(): void;
+        public update(): void;
+    }
 
-declare class PaginationEmbed {
-    /**
-     * Constructor for the Embed Paginator
-     * @param {Message} message A message object emitted from a messageCreate event coming from Eris, used as an invoker. 
-     * @param {EmbedOptions[]} pages An array containing all embed objects 
-     * @param {PaginationEmbedOptions} options An optional options object for overwriting defaults
-     */
-    constructor(message: Message, pages: EmbedOptions[], options?: PaginationEmbedOptions);
+    interface PaginationEmbedOptions {
+        showPageNumbers?: boolean;
+        extendedButtons?: boolean;
+        maxMatches?: number;
+        timeout?: number;
+        deleteButton?: string;
+        firstButton?: string;
+        lastButton?: string;
+        backButton?: string;
+        forthButton?: string;
+        startPage?: number;
+    }
 
-    /**
-     * Runs a set of initialization checks, sets up the reaction listener for continuous listening and displays the initial Embed
-     * @async
-     */
-
-    async initialize()
-
-    /**
-     * Updates the embed's content with the new page
-     */
-    update()
-
-    /**
-     * Main method handling the reaction listening and content updating
-     */
-    run()
-
-    /**
-        * Constructor for the Embed Paginator
-        * @param {Message} message A message object emitted from a messageCreate event coming from Eris, used as an invoker. 
-        * @param {EmbedOptions[]} pages An array containing all embed objects 
-        * @param {PaginationEmbedOptions} options An optional options object for overwriting defaults     
-    */
-
-    async createPaginationEmbed(message: Message, pages: EmbedOptions[], options: PaginationEmbedOptions)
+    export function createPaginationEmbed(message: Message, pages: EmbedOptions[], options?: PaginationEmbedOptions): Promise<void>;
 }
-
-interface PaginationEmbedOptions {
-    showPageNumbers?: boolean;
-    extendedButtons?: boolean;
-    maxMatches?: number;
-    timeout?: number;
-    deleteButton?: string;
-    firstButton?: string;
-    lastButton?: string;
-    backButton?: string;
-    forthButton?: string;
-    startPage?: number;
-}
-
-/**
-    * Constructor for the Embed Paginator
-    * @param {Message} message A message object emitted from a messageCreate event coming from Eris, used as an invoker. 
-    * @param {EmbedOptions[]} pages An array containing all embed objects 
-    * @param {PaginationEmbedOptions} options An optional options object for overwriting defaults     
-*/
-
-declare async function createPaginationEmbed(message: Message, pages: EmbedOptions[], options: PaginationEmbedOptions)
-
