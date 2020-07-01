a70fe9904d824d0ca402931a7b27cd6f4eefd607
whoastonic <kingstonicthe1@gmail.com>
2020-06-30 20:25:25 -0400

    debug and testing stuff

---
+++ tests/ActionButton.ts
@@ -0,0 +1,7 @@
+import test from 'ava'
+
+test('is working', (t) => {
+  const expected = 'is-working'
+
+  t.is('is-working', expected)
+})
---
+++ tests/PageBuilder.ts
@@ -0,0 +1,8 @@
+import test from 'ava'
+
+test('is working', (t) => {
+  const expected = 'is-working'
+
+  t.is('is-working', expected)
+})
+
